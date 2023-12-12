28317d39166fd00b28060c6de493551912e90bad
Aleh <aleh2806@yandex.ru>
2023-12-12 17:58:15 +0300

    работает все, как я хочу. Пока не сохраняется прогресс, жизни в Шаред преференсес и нет таймера

--- app/src/main/java/aleh/ahiyevich/trueorfalse/GameFragment.kt
+++ app/src/main/java/aleh/ahiyevich/trueorfalse/GameFragment.kt
@@ -2,8 +2,6 @@ package aleh.ahiyevich.trueorfalse
 
 import android.app.AlertDialog
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,12 +9,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
-import com.google.android.material.button.MaterialButton
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class GameFragment : Fragment() {
@@ -25,17 +19,17 @@ class GameFragment : Fragment() {
     private lateinit var health: TextView
     private lateinit var questionText: TextView
     private lateinit var commentText: TextView
+    private lateinit var btnTrue: Button
+    private lateinit var btnFalse: Button
+    private lateinit var btnNext: Button
+    private lateinit var btnCloseComment: Button
+    private var answer: Boolean = false
+
 
     private var counterHealth = 3
     private var counterQuestions = 0
 
     private var timerRunning = false
-    private var endTime = 0L
-    private var timeLeftInMillis = 0L
-
-
-    private var countDownTimer: CountDownTimer? = null
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,149 +43,106 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         addData()
 
-        var answer: Boolean
-        val btnTrue = requireActivity().findViewById<Button>(R.id.btn_true)
-        val btnFalse = requireActivity().findViewById<Button>(R.id.btn_false)
+        btnTrue = requireActivity().findViewById(R.id.btn_true)
+        btnFalse = requireActivity().findViewById(R.id.btn_false)
         health = requireActivity().findViewById(R.id.counter_health)
-        questionText = requireActivity().findViewById<TextView>(R.id.question)
+        questionText = requireActivity().findViewById(R.id.question)
         questionText.text = data[0].question
         health.text = counterHealth.toString()
 
-        btnTrue.setOnClickListener {
-            answer = true
-            checkButton(answer)
+
+        btnTrue.setOnClickListener { answer = true
+            checkAnswer(answer)
         }
 
-
-        btnFalse.setOnClickListener {
-            answer = false
-            checkButton(answer)
+        btnFalse.setOnClickListener { answer = false
+            checkAnswer(answer)
         }
     }
 
 
-    private fun checkButton(answer: Boolean) {
-        if (answer == data[counterQuestions].answer) {
-            Toast.makeText(requireContext(), "true", Toast.LENGTH_SHORT).show()
-            checkSizeQuestions()
-        } else {
-            checkHealth()
+    private fun checkAnswer(answer: Boolean) {
+            if (counterHealth > 0){
+                if (answer == data[counterQuestions].answer) {
+                    checkSizeQuestions()
+                } else {
+                    counterHealth--
+                    health.text = counterHealth.toString()
+                    Toast.makeText(requireContext(), "Health = $counterHealth", Toast.LENGTH_SHORT).show()
+                    if (counterHealth == 0){
+                        counterHealth = 0
+                        health.text = counterHealth.toString()
+                        openDialogTimer()
+                    }
+                }
+             } else {
+                counterHealth = 0
+                health.text = counterHealth.toString()
+                openDialogTimer()
+            }
         }
-    }
+
 
     private fun checkSizeQuestions() {
-        val view = View.inflate(requireContext(), R.layout.dialog_comment_layout, null)
-        val dialog = addDialog(view)
-        commentText = view.findViewById(R.id.dialog_comment)
 
-        if (counterQuestions == data.size - 1) {
-            dialog.show()
-            view.findViewById<Button>(R.id.btn_next).visibility = View.GONE
-            view.findViewById<Button>(R.id.btn_exit_comment).visibility = View.VISIBLE
+        if (counterQuestions != data.size - 1) {
+            openDialogComment()
             commentText.text = data[counterQuestions].comment
-            view.findViewById<Button>(R.id.btn_exit_comment).setOnClickListener {
-                dialog.dismiss()
-            }
+        } else {
+            openDialogComment()
+            btnNext.visibility = View.GONE
+            btnCloseComment.visibility = View.VISIBLE
+            commentText.text = data[counterQuestions].comment
             Toast.makeText(requireContext(), "last question", Toast.LENGTH_SHORT).show()
-        } else {
-            dialog.show()
-            commentText.text = data[counterQuestions].comment
-            view.findViewById<Button>(R.id.btn_next).setOnClickListener {
-                dialog.dismiss()
-                counterQuestions++
-                questionText.text = data[counterQuestions].question
-            }
         }
     }
 
-    private fun checkHealth() {
+
+    private fun openDialogTimer() {
         val view = View.inflate(requireContext(), R.layout.dialog_timer_layout, null)
-        val dialog = addDialog(view)
-
-        if (counterHealth > 1) {
-            Toast.makeText(requireContext(), "false, answer false - 1 life", Toast.LENGTH_SHORT)
-                .show()
-            counterHealth--
-            health.text = counterHealth.toString()
-        } else {
-            //Если закончились жизни, то открывается диалог с тайером
-            Toast.makeText(requireContext(), "open dialog", Toast.LENGTH_SHORT).show()
-            counterHealth = 0
-            health.text = counterHealth.toString()
-            dialog.show()
-            dialogDismissIfHealthZero(dialog, view)
-            playAds(view)
-        }
-    }
-
-    private fun dialogDismissIfHealthZero(dialog: AlertDialog, view: View) {
-        val btnClose = view.findViewById<ImageView>(R.id.btn_close)
-        val btnTrue = requireActivity().findViewById<Button>(R.id.btn_true)
-        val btnFalse = requireActivity().findViewById<Button>(R.id.btn_false)
-
-        btnTrue.setOnClickListener {
-            dialog.show()
-        }
-
-        btnFalse.setOnClickListener {
-            dialog.show()
-        }
-
-        btnClose.setOnClickListener {
-            dialog.dismiss()
-        }
-    }
-
-    private fun playAds(view: View){
-        val btnTrue = requireActivity().findViewById<Button>(R.id.btn_true)
-        val btnFalse = requireActivity().findViewById<Button>(R.id.btn_false)
-        val playAds = view.findViewById<Button>(R.id.play_ads)
-
-        playAds.setOnClickListener {
-            Toast.makeText(requireContext(), "play ads 15 sec", Toast.LENGTH_SHORT).show()
-            counterHealth++
-            health.text = counterHealth.toString()
-            if (counterHealth > 0){
-                var answer: Boolean
-                btnTrue.setOnClickListener {
-                    answer = true
-                    checkButton(answer)
-                }
-
-                btnFalse.setOnClickListener {
-                    answer = false
-                    checkButton(answer)
-                }
-            }
-        }
-    }
-
-
-    private fun addDialog(view: View): AlertDialog {
         val builder = AlertDialog.Builder(this.context)
         builder.setView(view)
         val dialog = builder.create()
         dialog.setCancelable(false)
         dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-        return dialog
+        dialog.show()
+
+        view.findViewById<ImageView>(R.id.btn_close).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        view.findViewById<Button>(R.id.play_ads).setOnClickListener {
+            Toast.makeText(requireContext(), "play ADS 15 sec", Toast.LENGTH_SHORT).show()
+            counterHealth++
+            health.text = counterHealth.toString()
+        }
     }
 
+    private fun openDialogComment() {
+        val view = View.inflate(requireContext(), R.layout.dialog_comment_layout, null)
+        val builder = AlertDialog.Builder(this.context)
+        builder.setView(view)
+        val dialog = builder.create()
+        dialog.setCancelable(false)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.show()
+        commentText = view.findViewById(R.id.dialog_comment)
+        btnNext = view.findViewById(R.id.btn_next)
+
+        btnNext.setOnClickListener {
+            counterQuestions++
+            questionText.text = data[counterQuestions].question
+            dialog.dismiss()
+        }
+
+        btnCloseComment = view.findViewById(R.id.btn_exit_comment)
+
+        btnCloseComment.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
 
     private fun addData() {
-        data.add(
-            DataBase(
-                "Действительно ли в году 12 месяцев?",
-                true,
-                "Да, действительно, в году 12 месяцев Да, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действителДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевДа, действительно, в году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdvв году 12 месяцевьно, в году 12 месяцевДа, действительно,vdsvsdvsdv"
-            )
-        )
-        data.add(
-            DataBase(
-                "Действительно ли солнце желтое?",
-                true,
-                "Да, действительно, солнце желтое"
-            )
-        )
 
         data.add(
             DataBase(
@@ -216,15 +167,6 @@ class GameFragment : Fragment() {
                 "Да, действительно, это 3"
             )
         )
-
-        data.add(
-            DataBase(
-                "Это 4?",
-                true,
-                "Да, действительно, это 4"
-            )
-        )
     }
-
 
 }
